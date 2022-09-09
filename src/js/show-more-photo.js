84d9b3a2e6ebc638676eed4223aa0ec9b9aa9d15
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-09 14:29:12 +0300

    Додає скрипт для книпки показу зображень

---
+++ src/js/show-more-photo.js
@@ -0,0 +1,17 @@
+const show = document.querySelector("[data-show]");
+const list = document.querySelector("[data-show-img]");
+
+let indexImg = 0;
+
+show.addEventListener("click", (Event) => {
+    event.preventDefault();
+
+    if (indexImg == 0) {
+        list.classList.remove("more-photo__list--show");
+        indexImg = 1;
+    } else {
+        list.classList.add("more-photo__list--show");
+        indexImg = 0;
+    }
+
+});
