59609090874c786b2c08fb0e49ac7765202deda4
Grimdot <107929069+Grimdot@users.noreply.github.com>
2022-09-12 10:22:42 +0000

    Deploying to gh-pages from @ Grimdot/project-team-1@da1a33dcf99c0b6290ee55184f50c5fe8242f159 🚀

---
+++ index.4e6574f6.js
@@ -0,0 +1,2 @@
+var show=document.querySelector(".more-photo__btn-show"),list=document.querySelector("[data-show-img]"),indexImg=0;show.addEventListener("click",(function(e){event.preventDefault(),0==indexImg?(list.classList.remove("more-photo__list--show"),indexImg=1):(list.classList.add("more-photo__list--show"),indexImg=0)}));
+//# sourceMappingURL=index.4e6574f6.js.map
--- index.f8170e68.js
+++
@@ -1,2 +0,0 @@
-var show=document.querySelector("[data-show]"),list=document.querySelector("[data-show-img]"),indexImg=0;show.addEventListener("click",(function(e){event.preventDefault(),0==indexImg?(list.classList.remove("more-photo__list--show"),indexImg=1):(list.classList.add("more-photo__list--show"),indexImg=0)}));
-//# sourceMappingURL=index.f8170e68.js.map
