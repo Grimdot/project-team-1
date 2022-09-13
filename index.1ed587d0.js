d6a7b2b6ab3f577c537d9af940033564000fe91a
Grimdot <107929069+Grimdot@users.noreply.github.com>
2022-09-13 18:06:46 +0000

    Deploying to gh-pages from @ Grimdot/project-team-1@456fece62ffdd4ed63d0471c5bfb1eb9a81e1592 🚀

---
+++ index.1ed587d0.js
@@ -0,0 +1,2 @@
+var langArr={".main-page-hero__title":{uk:"Інженерна, конструкторська та дизайнерська робота",en:"Engineering, construction and design work",ru:"Инженерная, конструкторская и дизайнерская работа"},".main-page-hero__text":{uk:"Ми втілюємо ідеї в реальність",en:"We turn ideas into reality",ru:"Мы воплощаем идеи в реальность"},".main-page-gallery__title":{uk:"ГАЛЕРЕЯ",en:"GALLERY",ru:"ГАЛЕРЕЯ"},".more-photo__section-title":{uk:"БІЛЬШЕ ФОТО",en:"MORE PHOTOS",ru:"БОЛЬШЕ ФОТО"}};
+//# sourceMappingURL=index.1ed587d0.js.map
---
+++ index.cf355c2f.js
@@ -0,0 +1,2 @@
+for(var key in langArr)document.querySelector(key).innerHTML=langArr[key][hash];
+//# sourceMappingURL=index.cf355c2f.js.map
