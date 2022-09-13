f7f119ca0a9b3bcf9787780f7cfadcf65d843be8
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-13 19:36:00 +0300

    Додає зміну мови для головної
    Поіншому реалізує перемикач мов
    
    працює у мазіли та не працюе в браузерах написаних на хромі

--- src/js/header-lang.js
+++ src/js/header-lang.js
@@ -26,41 +26,49 @@ const langLabel = Array.from(document.querySelectorAll('.lang__label'));
 
 const form = document.querySelector('.lang');
 
-const allLang = ["uk", "ru", "en"];
+const allLang = ['uk', 'ru', 'en'];
 
 let indexInput = 0;
-let hash = 'uk'
-console.log(indexInput);
+// let hash = 'uk';
+let hash = window.location.hash;
+hash = hash.substr(1);
 
 
+
+//Перевірка вибраної мови
 for (index = 0; index < langInput.length; ++index) {
-    langLabel[index].classList.add('visually-hidden');
-    
-    if (langInput[index].checked == true) {
-        console.log(langInput[index].checked);
-        langLabel[index].classList.remove('visually-hidden');
-        location.href = window.location.pathname + '#' + allLang[index];
-        hash = allLang[index];
-        document.documentElement.lang = hash;
-        indexInput = index;
+  langLabel[index].classList.add('visually-hidden');
 
+  if (langInput[index].checked == true) {
+      langLabel[index].classList.remove('visually-hidden');
+      hash = allLang[index];
+    location.href = window.location.pathname + '#' + hash;
+    document.documentElement.lang = hash;
+      indexInput = index;
+  } else {
+      langLabel[index].classList.add('visually-hidden');
     }
+    
 }
 
+// Вибір мови користувачем
 form.addEventListener('click', Event => {
-    if (indexInput < 2) {
+  if (indexInput < 2) {
     langInput[indexInput].checked = false;
     langInput[indexInput + 1].checked = true;
     langLabel[indexInput].classList.add('visually-hidden');
     langLabel[indexInput + 1].classList.remove('visually-hidden');
-    indexInput = indexInput + 1;
-      
+      indexInput = indexInput + 1;
+      hash = allLang[indexInput];
   } else if (indexInput == 2) {
     indexInput = 0;
     langInput[2].checked = false;
     langInput[indexInput].checked = true;
     langLabel[2].classList.add('visually-hidden');
-    langLabel[indexInput].classList.remove('visually-hidden');
-  }
+      langLabel[indexInput].classList.remove('visually-hidden');
+      hash = allLang[0];
+    }
+    // console.log(hash);
+    // window.location.hash =  '#' + hash;
     location.reload();
 });
---
+++ src/js/langIndex.js
@@ -0,0 +1,22 @@
+const langArr = {
+  '.main-page-hero__title': {
+    uk: 'Інженерна, конструкторська та дизайнерська робота',
+    en: 'Engineering, construction and design work',
+    ru: 'Инженерная, конструкторская и дизайнерская работа',
+  },
+  '.main-page-hero__text': {
+    uk: 'Ми втілюємо ідеї в реальність',
+    en: 'We turn ideas into reality',
+    ru: 'Мы воплощаем идеи в реальность',
+  },
+  '.main-page-gallery__title': {
+    uk: 'ГАЛЕРЕЯ',
+    en: 'GALLERY',
+    ru: 'ГАЛЕРЕЯ',
+  },
+  '.more-photo__section-title': {
+    uk: 'БІЛЬШЕ ФОТО',
+    en: 'MORE PHOTOS',
+    ru: 'БОЛЬШЕ ФОТО',
+  },
+};
